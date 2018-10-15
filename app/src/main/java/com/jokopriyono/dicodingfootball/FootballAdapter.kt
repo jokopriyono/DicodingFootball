2de00ecdb658c1a1ecce4f820da9d90f4bc0c1f7
jokopriyono <joe.mandrake007@gmail.com>
2018-10-16 00:02:18 +0700

    Initial commit

---
+++ app/src/main/java/com/jokopriyono/dicodingfootball/FootballAdapter.kt
@@ -0,0 +1,32 @@
+package com.jokopriyono.dicodingfootball
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import com.squareup.picasso.Picasso
+
+class FootballAdapter(private val context: Context, private val items: List<Item>, private val listener: (Item) -> Unit):
+        RecyclerView.Adapter<FootballAdapter.ViewHolder>() {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bindItem(items[position], listener)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+            ViewHolder(LayoutInflater.from(context).inflate(R.layout.item_football, parent, false))
+
+    override fun getItemCount(): Int = items.size
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view){
+        private val name = view.findViewById<TextView>(R.id.name)
+        private val image = view.findViewById<ImageView>(R.id.image)
+        fun bindItem(item: Item, listener: (Item) -> Unit){
+            name.text = item.name
+            item.image?.let { Picasso.get().load(it).into(image) }
+            itemView.setOnClickListener { listener(item) }
+        }
+    }
+}
