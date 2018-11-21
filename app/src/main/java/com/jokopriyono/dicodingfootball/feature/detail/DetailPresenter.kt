604ba49c85944f89210964eadb6012db552f8fa9
jokopriyono <joe.mandrake007@gmail.com>
2018-11-21 14:42:41 +0700

    - Implementation of coroutines

--- app/src/main/java/com/jokopriyono/dicodingfootball/feature/detail/DetailPresenter.kt
+++ app/src/main/java/com/jokopriyono/dicodingfootball/feature/detail/DetailPresenter.kt
@@ -4,19 +4,18 @@ import com.google.gson.Gson
 import com.jokopriyono.dicodingfootball.api.ApiRepository
 import com.jokopriyono.dicodingfootball.api.TheSportDBApi
 import com.jokopriyono.dicodingfootball.api.model.TeamResponse
-import org.jetbrains.anko.doAsync
-import org.jetbrains.anko.uiThread
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class DetailPresenter(val view: DetailView, private val repo: ApiRepository, private val gson: Gson) {
     fun getTwoTeams(team1: String?, team2: String?) {
         view.showLoading()
-        doAsync {
-            val dataHome = gson.fromJson(repo.doRequest(TheSportDBApi.getTeam(team1)), TeamResponse::class.java)
-            val dataAway = gson.fromJson(repo.doRequest(TheSportDBApi.getTeam(team2)), TeamResponse::class.java)
-            uiThread {
-                view.hideLoading()
-                view.showTeams(dataHome.teams[0], dataAway.teams[0])
-            }
+        GlobalScope.launch(Dispatchers.Main) {
+            val dataHome = gson.fromJson(repo.doRequest(TheSportDBApi.getTeam(team1)).await(), TeamResponse::class.java)
+            val dataAway = gson.fromJson(repo.doRequest(TheSportDBApi.getTeam(team2)).await(), TeamResponse::class.java)
+            view.hideLoading()
+            view.showTeams(dataHome.teams[0], dataAway.teams[0])
         }
     }
 }
--- app/src/main/java/com/jokopriyono/dicodingfootball/feature/main/MainPresenter.kt
+++ app/src/main/java/com/jokopriyono/dicodingfootball/feature/main/MainPresenter.kt
@@ -5,6 +5,9 @@ import com.jokopriyono.dicodingfootball.api.ApiRepository
 import com.jokopriyono.dicodingfootball.api.TheSportDBApi
 import com.jokopriyono.dicodingfootball.api.model.AllLeagueResponse
 import com.jokopriyono.dicodingfootball.api.model.LastLeagueResponse
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
 
@@ -12,34 +15,28 @@ class MainPresenter(val view: MainView, private val repo: ApiRepository, private
 
     fun getAllLeague() {
         view.showLoading()
-        doAsync {
-            val data = gson.fromJson(repo.doRequest(TheSportDBApi.getAllLeague()), AllLeagueResponse::class.java)
-            uiThread {
-                view.hideLoading()
-                view.showSpinner(data.leagues)
-            }
+        GlobalScope.launch(Dispatchers.Main) {
+            val data = gson.fromJson(repo.doRequest(TheSportDBApi.getAllLeague()).await(), AllLeagueResponse::class.java)
+            view.hideLoading()
+            view.showSpinner(data.leagues)
         }
     }
 
     fun getLastLeague(idLeague: Int) {
         view.showLoading()
-        doAsync {
-            val data = gson.fromJson(repo.doRequest(TheSportDBApi.getLastLeague(idLeague.toString())), LastLeagueResponse::class.java)
-            uiThread {
-                view.hideLoading()
-                view.showLastLeague(data.events)
-            }
+        GlobalScope.launch {
+            val data = gson.fromJson(repo.doRequest(TheSportDBApi.getLastLeague(idLeague.toString())).await(), LastLeagueResponse::class.java)
+            view.hideLoading()
+            view.showLastLeague(data.events)
         }
     }
 
     fun getNextLeague(idLeague: Int) {
         view.showLoading()
-        doAsync {
-            val data = gson.fromJson(repo.doRequest(TheSportDBApi.getNextLeague(idLeague.toString())), LastLeagueResponse::class.java)
-            uiThread {
-                view.hideLoading()
-                view.showLastLeague(data.events)
-            }
+        GlobalScope.launch {
+            val data = gson.fromJson(repo.doRequest(TheSportDBApi.getNextLeague(idLeague.toString())).await(), LastLeagueResponse::class.java)
+            view.hideLoading()
+            view.showLastLeague(data.events)
         }
     }
 }
