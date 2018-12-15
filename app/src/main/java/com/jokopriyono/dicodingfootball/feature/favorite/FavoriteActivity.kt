08bee38d6d0b5aefc4db8a2620f228d4f7e224d8
jokopriyono <joe.mandrake007@gmail.com>
2018-12-15 16:46:31 +0700

    - Menambahkan toolbar

--- app/src/main/java/com/jokopriyono/dicodingfootball/feature/favorite/FavoriteActivity.kt
+++ app/src/main/java/com/jokopriyono/dicodingfootball/feature/favorite/FavoriteActivity.kt
@@ -1,10 +1,13 @@
 package com.jokopriyono.dicodingfootball.feature.favorite
 
+import android.graphics.Color
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
 import android.support.v4.widget.SwipeRefreshLayout
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.Toolbar
 import android.widget.Toast
 import com.jokopriyono.dicodingfootball.R
 import com.jokopriyono.dicodingfootball.adapter.FavoriteAdapter
@@ -12,6 +15,7 @@ import com.jokopriyono.dicodingfootball.api.response.LastLeague
 import com.jokopriyono.dicodingfootball.data.FavoriteMatch
 import com.jokopriyono.dicodingfootball.data.database
 import org.jetbrains.anko.*
+import org.jetbrains.anko.appcompat.v7.themedToolbar
 import org.jetbrains.anko.db.classParser
 import org.jetbrains.anko.db.select
 import org.jetbrains.anko.recyclerview.v7.recyclerView
@@ -21,32 +25,42 @@ import org.jetbrains.anko.support.v4.swipeRefreshLayout
 class FavoriteActivity : AppCompatActivity(), FavoriteView {
     private var lastLeagues: MutableList<LastLeague> = mutableListOf()
     private lateinit var adapter: FavoriteAdapter
-    lateinit var swipeRefresh: SwipeRefreshLayout
     private lateinit var listEvent: RecyclerView
+    lateinit var swipeRefresh: SwipeRefreshLayout
+    lateinit var toolbar: Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        linearLayout {
-            lparams(width = matchParent, height = matchParent)
-            topPadding = dip(16)
-            leftPadding = dip(16)
-            rightPadding = dip(16)
-
-            swipeRefresh = swipeRefreshLayout {
-                setColorSchemeResources(R.color.colorAccent,
-                        android.R.color.holo_green_light,
-                        android.R.color.holo_orange_light,
-                        android.R.color.holo_red_light)
-
-                listEvent = recyclerView {
-                    id = R.id.recycler
-                    lparams(width = matchParent, height = matchParent)
-                    layoutManager = LinearLayoutManager(context)
-                }
+        verticalLayout {
+            toolbar = themedToolbar {
+                lparams(width = matchParent, height = wrapContent)
+                navigationIcon = ContextCompat.getDrawable(applicationContext, R.drawable.v_left)
+                backgroundColor = ContextCompat.getColor(applicationContext, R.color.colorPrimary)
             }
+            linearLayout {
+                lparams(width = matchParent, height = matchParent)
+                topPadding = dip(16)
+                leftPadding = dip(16)
+                rightPadding = dip(16)
 
+                swipeRefresh = swipeRefreshLayout {
+                    setColorSchemeResources(R.color.colorAccent,
+                            android.R.color.holo_green_light,
+                            android.R.color.holo_orange_light,
+                            android.R.color.holo_red_light)
+
+                    listEvent = recyclerView {
+                        id = R.id.recycler
+                        lparams(width = matchParent, height = matchParent)
+                        layoutManager = LinearLayoutManager(context)
+                    }
+                }
+
+            }
         }
+        toolbar.title = getString(R.string.favorites)
+        toolbar.setTitleTextColor(Color.WHITE)
 
         adapter = FavoriteAdapter(this, lastLeagues)
         listEvent.adapter = adapter
@@ -56,6 +70,8 @@ class FavoriteActivity : AppCompatActivity(), FavoriteView {
             lastLeagues.clear()
             showAllFavorite()
         }
+
+        toolbar.setNavigationOnClickListener { finish() }
     }
 
     override fun showAllFavorite() {
