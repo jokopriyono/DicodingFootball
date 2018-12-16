01a2871a34dc4cfc8bb528d98086ec2f0d9a56da
jokopriyono <joe.mandrake007@gmail.com>
2018-12-16 13:53:40 +0700

    - Show data to fragment

--- app/src/main/java/com/jokopriyono/dicodingfootball/feature/favorite/match/MatchFragment.kt
+++ app/src/main/java/com/jokopriyono/dicodingfootball/feature/favorite/match/MatchFragment.kt
@@ -3,18 +3,44 @@ package com.jokopriyono.dicodingfootball.feature.favorite.match
 import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.jokopriyono.dicodingfootball.adapter.LastLeagueAdapter
+import com.jokopriyono.dicodingfootball.api.model.LastLeagueResponse
+import com.jokopriyono.dicodingfootball.feature.favorite.FavoriteActivity
 import org.jetbrains.anko.*
 import org.jetbrains.anko.recyclerview.v7.recyclerView
 
 class MatchFragment : Fragment(), AnkoComponent<Context> {
     private lateinit var recycler: RecyclerView
+    private var lastLeagueResponse: LastLeagueResponse? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return createView(AnkoContext.create(requireContext()))
+        val view = createView(AnkoContext.create(requireContext()))
+
+        lastLeagueResponse?.let {
+            if (it.events.isNotEmpty()) {
+                context?.let { cntx ->
+                    recycler.layoutManager = LinearLayoutManager(cntx)
+                    val adapter = LastLeagueAdapter(cntx, it.events)
+                    recycler.adapter = adapter
+                }
+            }
+        }
+
+        return view
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val bundle: Bundle? = this.arguments
+        bundle?.let {
+            lastLeagueResponse = it.getParcelable(FavoriteActivity.BUNDLE_LAST)
+        }
     }
 
     override fun createView(ui: AnkoContext<Context>): View = with(ui) {
--- app/src/main/java/com/jokopriyono/dicodingfootball/feature/favorite/team/TeamFragment.kt
+++ app/src/main/java/com/jokopriyono/dicodingfootball/feature/favorite/team/TeamFragment.kt
@@ -3,18 +3,44 @@ package com.jokopriyono.dicodingfootball.feature.favorite.team
 import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.jokopriyono.dicodingfootball.adapter.TeamAdapter
+import com.jokopriyono.dicodingfootball.api.model.TeamResponse
+import com.jokopriyono.dicodingfootball.feature.favorite.FavoriteActivity
 import org.jetbrains.anko.*
 import org.jetbrains.anko.recyclerview.v7.recyclerView
 
 class TeamFragment : Fragment(), AnkoComponent<Context> {
     private lateinit var recycler: RecyclerView
+    private var teamResponse: TeamResponse? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return createView(AnkoContext.create(requireContext()))
+        val view = createView(AnkoContext.create(requireContext()))
+
+        teamResponse?.let {
+            if (it.teams.isNotEmpty()) {
+                context?.let { cntx ->
+                    recycler.layoutManager = LinearLayoutManager(cntx)
+                    val adapter = TeamAdapter(cntx, it.teams)
+                    recycler.adapter = adapter
+                }
+            }
+        }
+
+        return view
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val bundle: Bundle? = this.arguments
+        bundle?.let {
+            teamResponse = it.getParcelable(FavoriteActivity.BUNDLE_TEAM)
+        }
     }
 
     override fun createView(ui: AnkoContext<Context>): View = with(ui) {
